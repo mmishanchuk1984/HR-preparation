ca43d0fe63462ecc411404ac5643fba575803ce2
mmishanchuk1984 <mmishanchuk1984@gmail.com>
2022-06-10 23:14:35 +0300

    test

---
+++ tupples/tuple1.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-tuples/problem?isFullScreen=true#:~:text=Tutorial-,Task%0AGiven%20an%20integer%2C,.,-Sample%20Input%200
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    t = tuple(integer_list)
+    hash_num = str(hash(t))
+    print(hash_num)
+
